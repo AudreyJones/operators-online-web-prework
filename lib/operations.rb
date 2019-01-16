9df59573e3bd80b368a32c15bdaf1271149470ae
Audrey Jones <audrey.eliz.jones@gmail.com>
2019-01-16 14:47:21 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 def unsafe?(speed)
   if ((speed < 40) || (speed > 60))
     return true
@@ -5,7 +7,7 @@ def unsafe?(speed)
     return false
   end
 end
-
+binding.pry
 
 
 def not_safe?(speed)
